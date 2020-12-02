be75db47a420295c921ff3325647fde34cacb010
Lee Biddiscombe (Co-op IT - Retail) <lee.biddiscombe@coop.co.uk>
2020-12-02 12:18:59 +0000

    added a public link

--- src/components/Login.svelte
+++ src/components/Login.svelte
@@ -14,37 +14,43 @@
 </script>
 
 <div class="grid w-full h-full place-items-center">
-  <div class="w-full max-w-xs p-5 mx-auto bg-indigo-100 rounded">
+  <div class="w-full max-w-xs p-5 mx-auto bg-gray-900 rounded">
     <header>
       <img class="w-20 mx-auto mb-5" src="https://img.icons8.com/bubbles/344/brain.png" alt="Brain logo" />
+      <h1 class="mb-8 -mt-8 text-xl text-center">InspireME</h1>
     </header>
+
     <form>
       <div>
-        <label class="block mb-2 text-indigo-500" for="username">Username</label>
+        <label class="block mb-2 text-gray-500" for="username">Username</label>
         <input
-          class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+          class="w-full p-2 mb-4 text-indigo-700 bg-gray-300 outline-none focus:bg-white"
           type="text"
           name="username"
           bind:value={username} />
       </div>
       <div>
-        <label class="block mb-2 text-indigo-500" for="password">Password</label>
+        <label class="block mb-2 text-gray-500" for="password">Password</label>
         <input
-          class="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
+          class="w-full p-2 mb-4 text-indigo-700 bg-gray-300 outline-none focus:bg-white"
           type="password"
           name="password"
           bind:value={password} />
       </div>
       <div>
         <button
-          on:click={handleSubmit}
-          type="button"
-          class="w-full px-4 py-2 mb-6 font-bold text-white bg-indigo-700 rounded hover:bg-pink-700">{isSignInMode ? 'Sign In' : 'Sign Up'}</button>
+          on:click|preventDefault={handleSubmit}
+          type="submit"
+          class="w-full px-4 py-2 mb-6 font-bold text-white bg-green-700 rounded hover:bg-green-500">{isSignInMode ? 'Sign In' : 'Sign Up'}</button>
       </div>
     </form>
     <button
-      class="float-right px-4 py-2 mb-6 font-bold text-gray-700 underline border-0 outline-none"
+      class="w-full px-4 py-2 font-bold text-center text-gray-600 underline border-0 outline-none"
       on:click={() => (isSignInMode = !isSignInMode)}
-      type="button">{isSignInMode ? 'Create a user' : 'Sign in existing user'}</button>
+      type="button">{isSignInMode ? 'Create a new user' : 'Sign in existing user'}</button>
+    <div class="h-px my-4 bg-gray-700" />
+    <button
+      on:click={() => signIn('public', 'public')}
+      class="w-full px-4 py-2 font-bold text-white bg-indigo-700 rounded hover:bg-green-500">Try the Public board</button>
   </div>
 </div>
